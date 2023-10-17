45ae44ba2a4edcd55e96c291acc0f382ff19ec96
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-10-16 19:44:39 -0500

    Coding with Video
    coding with video

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,12 +11,7 @@ export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
 
-  constructor(private recipeService: RecipeService) { }
-
   ngOnInit() {
-  }
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
   }
