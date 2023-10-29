06855fa28e8f3a6bfbbd8b0b7acf44a09caf4286
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-10-29 18:32:54 -0500

    Coding with Video
    coding with video

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,25 +9,26 @@ import { Subject } from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say?',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat',1)
-      ])
-  ];
+ // private recipes: Recipe[] = [
+  //  new Recipe(
+   //   'Tasty Schnitzel',
+   //   'A super-tasty Schnitzel - just awesome!',
+  //    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+   //   [
+   //     new Ingredient('Meat', 1),
+   //     new Ingredient('French Fries', 20)
+   //   ]),
+   // new Recipe(
+    //  'Big Fat Burger',
+    //  'What else you need to say?',
+    //  'https://cdn.pixabay.com/photo/2016/06/15/19/09///food-1459693_960_720.jpg',
+     // [
+     //   new Ingredient('Buns', 2),
+     //   new Ingredient('Meat',1)
+     // ])
+ // ];
 
+ private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -15,7 +16,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-1b815-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
+    this.http
+    .get<Recipe[]>(
+      'https://ng-course-recipe-book-1b815-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+      });
+    }))
+    .subscribe(recipes => {
       this.recipeService.setRecipes(recipes);
     });
   }
