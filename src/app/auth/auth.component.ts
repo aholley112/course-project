0f00707f7e39dce7b1cf84fb6d3f1f23b7b39762
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-11-02 20:41:01 -0500

    Coding with Video
    coding with video

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,7 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-auth',
-  templateURL: './auth.component.html'
+  templateUrl: './auth.component.html'
 })
 
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
