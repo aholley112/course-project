5219bf6c7e74f2be6d1dcf36a2172a94b4598c09
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-11-08 08:55:00 -0600

    Coding with Video
    coding with video

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,9 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 
 @NgModule({
@@ -16,6 +19,7 @@ import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   exports: [
     RecipesComponent,
     RecipeListComponent,
