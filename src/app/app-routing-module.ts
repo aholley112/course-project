56f809c7081f047bd2bd0ff60ea532f2437c64be
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-11-08 10:04:37 -0600

    Coding with Video
    coding with video

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+  { path: 'recipes', loadChildren: './recipes/recipes.module.#RecipesModule' },
   {
     path: 'shopping-list',
     loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'
